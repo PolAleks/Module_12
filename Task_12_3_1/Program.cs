4e52b2f55190be63f5c87ccaffe80c3c4b8c1a41
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-07 17:01:56 +0300

    Поиск элемента в отсортированном массиве

--- Task_12_3_1/Program.cs
+++ Task_12_3_1/Program.cs
@@ -4,25 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 2, 8, 3, 6, 3, 1, 9 };
+            int[] array = { 0, 1, 2, 4, 5, 7, 9, 10, 11, 12, 13, 14, 15 };
             int position = BinarySearch(3, array, 0, array.Length);
             Console.WriteLine(position);
         }
-
-        static int BinarySearch(int value, int[] array, int start, int end)
+        /// <summary>
+        /// Метод бинарного поиска в массиве
+        /// </summary>
+        /// <param name="value">Искомое значение</param>
+        /// <param name="array">Массив в котором необходимо найти искомое значение</param>
+        /// <param name="left">Стартовый индекс поиска</param>
+        /// <param name="right">Конечный индекс поиска</param>
+        /// <returns>Возвращает индекс искомого элемента</returns>
+        static int BinarySearch(int value, int[] array, int left, int right)
         {
-            Array.Sort(array);
-            int middle = 0;
-
-            while (array[middle] != value)
+            while (left <= right)
             {
-                middle = (start + end) / 2;
-                if (value < array[middle])
-                    end = middle - 1;
-                else if(value > array[middle])
-                    start = middle + 1;                
+                var middle = (left + right) / 2;
+
+                var midElement = array[middle];
+
+                if (midElement == value)
+                    return middle;
+                else if (value < midElement)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
             }
-            return middle;
+
+            return -1;
         }
     }
 }
---
+++ Task_12_3_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace Task_12_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =  { 0, 1, 2, 4, 5, 7, 9, 10, 11, 12, 13, 14, 15 };
+            int searchIndex = FindIndexElement(array, 4);
+
+            Console.WriteLine($"Индекс вставки элемента - {searchIndex}");
+        }
+        static int FindIndexElement(int[] array, int insertionValue)
+        {
+            int index;
+            for (index = 0; index < array.Length; index++)
+            {
+                if (array[index] > insertionValue)
+                    return index;
+            }
+
+            return index;
+        }
+
+        static void ShowArray(int[] array)
+        {
+            foreach(var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
